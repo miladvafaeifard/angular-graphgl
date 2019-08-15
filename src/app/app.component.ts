66250e19559819c6ad1a68fbecf33468d8b2d8bf
milad <milad.vafaeifard@gmail.com>
2019-08-15 17:58:47 +0200

    implement apollo graphql

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Apollo } from 'apollo-angular'
+import { Observable } from 'rxjs'
+
+import gql from 'graphql-tag'
+import { map } from 'rxjs/operators'
+import { Mutation, Post, Query } from './types'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Angular Apollo-GraphQL'
+  posts: Observable<Post[]>
+
+  constructor(private apollo: Apollo) {
+  }
+
+  ngOnInit(): void {
+    this.posts = this.apollo.watchQuery<Query>({
+      query: gql`
+          query allPosts {
+              posts {
+                  id
+                  title
+                  votes
+                  author {
+                      id
+                      firstName
+                      lastName
+                  }
+              }
+          }
+      `,
+    })
+    .valueChanges
+    .pipe(
+      map((res: any) => res.data.posts),
+    )
+  }
+
+  public voteUp(id) {
+    this.apollo.mutate<Mutation>({
+      mutation: gql`
+          mutation upvotePost($postId: Int!) {
+              upvotePost(postId: $postId) {
+                  id
+                  votes
+              }
+          }
+      `,
+      variables: {
+        postId: id,
+      },
+    }).subscribe()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { GraphQLModule } from './graphql.module'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
